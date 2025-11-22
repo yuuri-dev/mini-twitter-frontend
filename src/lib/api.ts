7d3a1b9a3f87a4cadf0d8cc5a3b8428dbfce6955
Yuuri Inoue <yuuri041125@gmail.com>
2025-11-23 04:32:14 +0900

    set up

---
+++ src/lib/api.ts
@@ -0,0 +1,12 @@
+// src/lib/api.ts
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
