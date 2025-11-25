aff16d03193d1b08446b24903943c10515d8e3bb
Yuuri Inoue <yuuri041125@gmail.com>
2025-11-25 17:22:37 +0900

    fix

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
-    <div className='w-full h-12 bg-blue-500 flex items-center justify-center'><h1 className='text-xl text-white'>Twitter</h1></div>
-  )
-}
+    <div className="w-full h-12 bg-blue-500 flex items-center justify-center">
+      <h1 className="text-xl text-white">Twitter</h1>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
