3970671328a2ad6a2ba59e29fcfe5c08253727f1
Yuuri Inoue <yuuri041125@gmail.com>
2025-11-25 17:22:11 +0900

    feat:Headerを実装

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-full h-12 bg-blue-500 flex items-center justify-center'><h1 className='text-xl text-white'>Twitter</h1></div>
   )
 }
 
