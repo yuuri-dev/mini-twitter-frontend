346de619632996c61f646ec0829b3e1235c84dbf
Yuuri Inoue <yuuri041125@gmail.com>
2025-11-28 09:59:18 +0900

    feat:途中までprofileを実装

--- src/app/(protected)/profile/me/page.tsx
+++ src/app/(protected)/profile/me/page.tsx
@@ -2,16 +2,24 @@
 import { api } from '@/lib/api';
 import React, { useEffect, useState } from 'react';
 
+type myTweets = {
+  id: number;
+  contents: string;
+  createdAt: string;
+};
+
 const Me = () => {
   const [userName, setUserName] = useState('');
+  const [userTweets, setUserTweets] = useState([]);
   useEffect(() => {
     const getUserData = async () => {
       const userData = await api.get('/users/me');
       setUserName(userData.data.email);
       const myTweets = await api.get('/tweets/me');
+      setUserTweets(myTweets.data);
     };
     getUserData();
-  });
+  }, []);
   return (
     <div>
       <p>profile</p>
@@ -20,7 +28,13 @@ const Me = () => {
       <p>{userName}</p>
 
       <p>過去のツイート</p>
-      <p>{ }</p>
+
+      {userTweets &&
+        userTweets.map((tweet: myTweets) => (
+          <div key={tweet.id}>
+            <p>{tweet.contents}</p>
+          </div>
+        ))}
     </div>
   );
 };
