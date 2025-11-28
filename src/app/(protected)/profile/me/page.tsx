dd5855ff67cf918c15091354c276f955f7f922ad
Yuuri Inoue <yuuri041125@gmail.com>
2025-11-28 09:46:21 +0900

    fix

--- src/app/(protected)/profile/me/page.tsx
+++ src/app/(protected)/profile/me/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client';
+import { api } from '@/lib/api';
+import React, { useEffect, useState } from 'react';
 
 const Me = () => {
+  const [userName, setUserName] = useState('');
+  useEffect(() => {
+    const getUserData = async () => {
+      const userData = await api.get('/users/me');
+      setUserName(userData.data.email);
+      const myTweets = await api.get('/tweets/me');
+    };
+    getUserData();
+  });
   return (
-    <div>Me</div>
-  )
-}
+    <div>
+      <p>profile</p>
+      <p>ユーザー名</p>
+      <p>メールアドレス</p>
+      <p>{userName}</p>
 
-export default Me
+      <p>過去のツイート</p>
+      <p>{ }</p>
+    </div>
+  );
+};
+
+export default Me;
