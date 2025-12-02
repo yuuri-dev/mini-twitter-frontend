18f1a59cf0ea689e472d800b441ca6f2de995dc8
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-02 23:56:17 +0900

    feat:topぺーじにheaderを追加

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Header from '@/components/layout/header';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import React from 'react';
@@ -7,6 +8,7 @@ const Top = () => {
   const router = useRouter()
   return (
     <div>
+      <Header />
       <p>topページです</p>
       <Button onClick={() => router.push('/auth/login')}>ログイン</Button>
       <Button onClick={() => router.push('/auth/signup')}>新規登録</Button>
