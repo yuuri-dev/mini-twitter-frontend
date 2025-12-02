0d996807cbfabba99c1672e2f7f2fe4e8090d749
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-03 00:23:11 +0900

    feat:topページのデザインを実装

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,13 +5,34 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Top = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div>
       <Header />
-      <p>topページです</p>
-      <Button onClick={() => router.push('/auth/login')}>ログイン</Button>
-      <Button onClick={() => router.push('/auth/signup')}>新規登録</Button>
+      <div className="w-full md:grid grid-cols-2 gap-3">
+        <div className="h-screen flex justify-center items-center">
+          <img
+            src="twitter_icon.png"
+            alt="icon"
+            className="md:w-[300px] md:h-[300px]"
+          />
+        </div>
+        <div className="mt-24">
+          <h1 className="text-6xl font-bold">すべての話題が、ここに。</h1>
+          <h2 className="text-3xl font-semibold mt-15">
+            今すぐ参加しましょう。
+          </h2>
+          <p className="text-xl font-midium mt-12">新規登録はこちらから</p>
+          <Button onClick={() => router.push('/auth/signup')}>
+            アカウントを作成
+          </Button>
+          <p className="font-thin text-xs w-80 my-3">
+            アカウントを登録することにより、利用規約とプライバシーポリシー（Cookieの使用を含む）に同意したとみなされます。
+          </p>
+          <p className="text-xl font-midium mt-12">アカウントをお持ちの方</p>
+          <Button onClick={() => router.push('/auth/login')}>ログイン</Button>
+        </div>
+      </div>
     </div>
   );
 };
