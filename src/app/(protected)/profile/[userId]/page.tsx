b1c3376919bb428bd6e3e9e87f76a2f68dc63b98
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-07 23:22:24 +0900

    fix

---
+++ src/app/(protected)/profile/[userId]/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>プロフィールページ</h1>
+      <p>User ID: </p>
+    </div>
+  );
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
