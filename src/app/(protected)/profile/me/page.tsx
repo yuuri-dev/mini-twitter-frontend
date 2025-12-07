472935eba0eec0199aa8ecce71c1db1b42f9dfd8
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-07 21:43:21 +0900

    fix

--- src/app/(protected)/profile/me/page.tsx
+++ src/app/(protected)/profile/me/page.tsx
@@ -3,41 +3,67 @@ import { api } from '@/lib/api';
 import React, { useEffect, useState } from 'react';
 import { formatRelativeTime } from '@/lib/utils';
 
-type myTweets = {
+type MyTweet = {
   id: number;
   content: string;
   createdAt: string;
 };
+
+type User = {
+  email: string;
+  userName: string;
+};
+
 const Me = () => {
-  const [userName, setUserName] = useState('');
-  const [userTweets, setUserTweets] = useState([]);
+  const [user, setUser] = useState<User | null>(null);
+  const [tweets, setTweets] = useState<MyTweet[]>([]);
+
   useEffect(() => {
-    const getUserData = async () => {
-      const userData = await api.get('/users/me');
-      setUserName(userData.data.email);
-      const myTweets = await api.get('/tweets/me');
-      setUserTweets(myTweets.data);
+    const fetchData = async () => {
+      const userRes = await api.get('/users/me');
+      setUser({
+        email: userRes.data.email,
+        userName: userRes.data.userName,
+      });
+
+      const tweetsRes = await api.get('/tweets/me');
+      setTweets(tweetsRes.data);
     };
-    getUserData();
+
+    fetchData();
   }, []);
+
+  if (!user) return <p>Loading...</p>;
+
   return (
-    <div>
-      <p>profile</p>
-      <p>ユーザー名</p>
-      <p>メールアドレス</p>
-      <p>{userName}</p>
+    <div className="w-full m-10">
+      <div className="m-auto w-fit space-y-2">
+        <div className="flex gap-10">
+          <p>ユーザー名</p>
+          <p className="">{user.userName}</p>
+        </div>
 
-      <h2 className="text-2xl">過去のツイート</h2>
+        <div className="flex gap-10">
+          <p>メールアドレス</p>
+          <p>{user.email}</p>
+        </div>
+      </div>
 
-      {userTweets &&
-        userTweets.map((tweet: myTweets) => (
-          <div key={tweet.id}>
+      <div className="mt-10 max-w-[500px] m-auto">
+        <h2 className="text-2xl text-center">過去のツイート</h2>
+
+        {tweets.map((tweet) => (
+          <div
+            key={tweet.id}
+            className="flex justify-between m-5 border-b pb-3"
+          >
             <p>{tweet.content}</p>
             <p className="text-gray-500 text-sm">
               {formatRelativeTime(tweet.createdAt)}
             </p>
           </div>
         ))}
+      </div>
     </div>
   );
 };
