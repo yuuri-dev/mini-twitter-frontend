5bd3427f30b2ae1acac7e9c5df6e2e58c7c1650a
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-10 17:02:47 +0900

    feat:footerを実装

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -9,23 +9,25 @@ const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
 
   return (
-    <div className="h-screen relative">
+    <div className="min-h-screen relative flex flex-col">
       <Header onToggleSidebar={toggleSidebar} />
 
-      {isSidebarOpen && (
-        <>
-          <div
-            className="fixed inset-0 bg-black/50 z-40"
-            onClick={toggleSidebar}
-          />
+      <main className="flex-1 relative">
+        {isSidebarOpen && (
+          <>
+            <div
+              className="fixed inset-0 bg-black/50 z-40"
+              onClick={toggleSidebar}
+            />
 
-          <div className="fixed inset-y-0 left-0 z-50 w-64">
-            <Sidebar onToggleSidebar={toggleSidebar} />
-          </div>
-        </>
-      )}
+            <div className="fixed inset-y-0 left-0 z-50 w-64">
+              <Sidebar onToggleSidebar={toggleSidebar} />
+            </div>
+          </>
+        )}
 
-      <div className="flex-1">{children}</div>
+        {children}
+      </main>
       <Footer />
     </div>
   );
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full py-4 text-center text-sm text-gray-500 bg-gray-600 text-white">
+      &copy; 2025 twitter
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
