b52322f74a4a477d12556140baed2d3e2693e223
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-10 13:36:26 +0900

    feat:login/signupぺーじにheaderを追加

--- src/app/(public)/auth/login/page.tsx
+++ src/app/(public)/auth/login/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Header from '@/components/layout/header';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { api } from '@/lib/api';
@@ -29,27 +30,30 @@ const LoginPage = () => {
   }
 
   return (
-    <div className="max-w-[600px] m-auto my-10 px-10">
-      <h1 className="text-center mb-3 text-3xl text-bold">ログイン</h1>
+    <>
+      <Header />
+      <div className="max-w-[600px] m-auto my-10 px-10">
+        <h1 className="text-center mb-3 sm:text-3xl text-2xl text-bold">ログイン</h1>
 
-      <form className="space-y-4" onSubmit={handleLogin}>
-        <Input
-          type="email"
-          placeholder="メールアドレス"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="パスワード"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="text-center">
-          <Button type="submit">ログイン</Button>
-        </div>
-      </form>
-    </div>
+        <form className="space-y-4" onSubmit={handleLogin}>
+          <Input
+            type="email"
+            placeholder="メールアドレス"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="パスワード"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="text-center">
+            <Button type="submit">ログイン</Button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/app/(public)/auth/signup/page.tsx
+++ src/app/(public)/auth/signup/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Header from '@/components/layout/header';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { api } from '@/lib/api';
@@ -31,33 +32,38 @@ const SignUpPage = () => {
   }
 
   return (
-    <div className="max-w-[600px] m-auto my-10 px-10">
-      <h1 className="text-center mb-3 text-3xl text-bold">アカウントを作成</h1>
+    <>
+      <Header />
+      <div className="max-w-[600px] m-auto my-10 px-10">
+        <h1 className="text-center mb-3 sm:text-3xl text-2xl text-bold">
+          アカウントを作成
+        </h1>
 
-      <form className="space-y-4" onSubmit={handleLogin}>
-        <Input
-          type="text"
-          placeholder="ユーザー名"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <Input
-          type="email"
-          placeholder="メールアドレス"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="パスワード"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="text-center">
-          <Button type="submit">登録</Button>
-        </div>
-      </form>
-    </div>
+        <form className="space-y-4" onSubmit={handleLogin}>
+          <Input
+            type="text"
+            placeholder="ユーザー名"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <Input
+            type="email"
+            placeholder="メールアドレス"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="パスワード"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="text-center">
+            <Button type="submit">登録</Button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
