2e5f8108fbd00951ef3215644f41e9430b88911f
Yuuri Inoue <yuuri041125@gmail.com>
2025-12-11 23:57:57 +0900

    feat:not-foundページを実装

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const NotFound = () => {
+    const router = useRouter();
+    return (
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      <h1 className="text-bold text-4xl pb-3">404</h1>
+          <p className="text-bold text-2xl pb-1.5"> ページが見つかりませんでした。</p>
+            <Button onClick={(() => router.push('/'))}>Topに戻る</Button>
+    </div>
+  );
+};
+
+export default NotFound;
