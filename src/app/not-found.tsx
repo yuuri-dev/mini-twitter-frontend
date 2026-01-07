94447c779982f9e8df22ab6eb923a45d250c74c3
Yuuri Inoue <yuuri041125@gmail.com>
2026-01-07 18:16:53 +0900

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     const router = useRouter();
     return (
     <div className='min-h-screen flex flex-col justify-center items-center'>
-      <h1 className="text-bold text-4xl pb-3">404</h1>
+      <h1 className="text-bold text-4xl pb-3">404 Not Found</h1>
           <p className="text-bold text-2xl pb-1.5"> ページが見つかりませんでした。</p>
             <Button onClick={(() => router.push('/'))}>Topに戻る</Button>
     </div>
